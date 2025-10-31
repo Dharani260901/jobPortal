f3e07e2279940b180d0c2223f19749ec6a22cd53
Dharani Malairaj <dharanimalairaj26@gmail.com>
2025-10-31 11:55:55 +0530

    Update CORS to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://job-portal-application-liard.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
